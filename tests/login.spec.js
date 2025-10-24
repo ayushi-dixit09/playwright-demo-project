2137bac4565d5c3bd81d4dfe3d9e69f4ad622a7f
Ayushi <ayushidixit4@gmail.com>
2025-10-24 07:20:39 +0100

    updated spec file for best practice

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,12 +4,12 @@ test('valid login functionality',async({page})=>{
     const login= new LoginPage(page);
 
     //test data
-    const username='aayush234';
-    const password='mypassword';
+   // const username='aayush234';
+   // const password='mypassword';
     
     
     await login.openWebsite();
     await login.openLoginPage();
-    await login.Login(username,password); // fill username ,password and submit login
+    await login.Login('aayush234','mypassword'); // fill username ,password and submit login
     await login.verifySuccessful(); // verify partial match
 })
